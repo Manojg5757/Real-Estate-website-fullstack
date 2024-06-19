092f84754099f73d59a5561707d16ffa2727ea1a
manojvsp <gmanojece2021@gmail.com>
2024-06-19 14:57:45 +0530

    created database in mongodb cloud and connected it with backend

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,42 +1,39 @@
-import React from 'react'
+import React from "react";
 import { IoSearch } from "react-icons/io5";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <header className='bg-slate-200 shadow-md'>
-      <div className='flex justify-between items-center max-w-6xl mx-auto p-3'>
-      <Link to='/'>
-      <h1 className='flex flex-wrap text-sm sm:text-xl '>
-        <span className='text-slate-500'>Kalees</span>
-        <span className='text-slate-700'>Estate</span>
-      </h1>
-      </Link>
-      <form className='bg-slate-100 p-2 rounded-lg flex items-center' >
-        <input type="text" placeholder='Search...' className='bg-transparent outline-none w-24 sm:w-64 ' />
-        <IoSearch />
-      </form>
-      <ul className='flex gap-4'>
-        <li className='hidden sm:inline hover:underline  text-slate-700'>
-          <Link to='/'>
-          Menu
-          </Link>
-          </li> 
-        <li className='hidden sm:inline hover:underline text-slate-700'>
-        <Link to='/signout'>
-          SignOut
-          </Link>
-        </li>
-        <li className='hover:underline text-slate-700'>
-        <Link to='/signin'>
-          Signin
-          </Link>
-        </li>
-      </ul>
+    <header className="bg-slate-200 shadow-md">
+      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+        <Link to="/">
+          <h1 className="flex flex-wrap text-sm sm:text-xl ">
+            <span className="text-slate-500">Kalees</span>
+            <span className="text-slate-700">Estate</span>
+          </h1>
+        </Link>
+        <form className="bg-slate-100 p-2 rounded-lg flex items-center">
+          <input
+            type="text"
+            placeholder="Search..."
+            className="bg-transparent outline-none w-24 sm:w-64 "
+          />
+          <IoSearch />
+        </form>
+        <ul className="flex gap-4">
+          <li className="hidden sm:inline hover:underline  text-slate-700">
+            <Link to="/">Menu</Link>
+          </li>
+          <li className="hidden sm:inline hover:underline text-slate-700">
+            <Link to="/signout">SignOut</Link>
+          </li>
+          <li className="hover:underline text-slate-700">
+            <Link to="/signin">Signin</Link>
+          </li>
+        </ul>
       </div>
-      
     </header>
-  )
-}
+  );
+};
 
-export default Header
+export default Header;
--- server/index.js
+++ server/index.js
@@ -1,22 +1,26 @@
-import express from 'express'
-import cors from 'cors'
-import mongoose from 'mongoose'
+import express from "express";
+import cors from "cors";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+dotenv.config();
+const app = express();
 
+app.use(express.json());
+app.use(cors());
 
-const app = express()
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Connected to mongodb");
+  })
+  .catch((err) => console.log(err));
 
-app.use(express.json())
-app.use(cors())
+const port = 3000;
 
+app.get("/", (req, res) => {
+  res.json({ message: "Manoj" });
+});
 
-
-const port = 3000
-
-app.get('/',(req,res)=>{
-    res.json({message:"Manoj"})
-})
-
-app.listen(port,()=>{
-    console.log("Connected to server")
-})
-
+app.listen(port, () => {
+  console.log("Connected to server");
+});
