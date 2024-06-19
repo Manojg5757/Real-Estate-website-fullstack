1f88a21111994632feeb0799ef04d1e99f47c0ae
manojvsp <gmanojece2021@gmail.com>
2024-06-19 13:47:56 +0530

    created routes for pages using react router dom

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Home from './pages/Home'
+import Profile from './pages/Profile'
+import About from './pages/About'
+import Signin from './pages/Signin'
+import SignOut from './pages/SignOut'
 
 const App = () => {
   return (
-    <div>App</div>
+   <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/profile' element={<Profile />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/signin' element={<Signin />} />
+        <Route path='/signout' element={<SignOut />} />
+      </Routes>
+   </BrowserRouter>
   )
 }
 
