d983b290a679e19168263090d6315b52260ed868
manojvsp <gmanojece2021@gmail.com>
2024-06-26 21:46:27 +0530

    update header search form

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,10 +1,25 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { IoSearch } from "react-icons/io5";
-import { Link } from "react-router-dom";
+import { Link,useNavigate } from "react-router-dom";
 import {useSelector} from 'react-redux'
 
 const Header = () => {
   const {currentUser} = useSelector(state=>state.user)
+  const[searchTerm,setSearchTerm] = useState('')
+  const navigate = useNavigate()
+  const handleSubmit = (e)=>{
+    e.preventDefault();
+    const urlParams = new URLSearchParams(window.location.search)
+    urlParams.set('searchTerm',searchTerm)
+    const searchQuery = urlParams.toString()
+    navigate(`/search?${searchQuery}`)
+  }
+
+  useEffect(()=>{
+   const urlParams = new URLSearchParams(location.search)
+   const searchTermFromUrl = urlParams.get('searchTerm')
+   setSearchTerm(searchTermFromUrl)
+  },[location.search])
   return (
     <header className="bg-slate-200 shadow-md">
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
@@ -14,13 +29,17 @@ const Header = () => {
             <span className="text-slate-700">Estate</span>
           </h1>
         </Link>
-        <form className="bg-slate-100 p-2 rounded-lg flex items-center">
+        <form onSubmit={handleSubmit} className="bg-slate-100 p-2 rounded-lg flex items-center">
           <input
             type="text"
             placeholder="Search..."
+            value={searchTerm}
             className="bg-transparent outline-none w-24 sm:w-64 "
+            onChange={(e)=>setSearchTerm(e.target.value)}
           />
+          <button type="submit">
           <IoSearch />
+          </button>
         </form>
         <ul className="flex gap-4">
           <li className="hidden sm:inline hover:underline  text-slate-700">
